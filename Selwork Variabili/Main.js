c4596222d1fc5828d2bdacbceeea8c43c23743a5
FaustNFurious <lorelego2003@gmail.com>
2024-11-09 21:31:06 +0100

    Add files via upload
    Svolto esercizio variabili javascript

---
+++ Selwork Variabili/Main.js
@@ -0,0 +1,33 @@
+// Dichiarazione num1
+let num1 = 1;
+
+// Dichiarazione num1
+let num2 = 10;
+
+// Visualizzazione num1
+console.log('Il valore della variabile num1 è ' + num1);
+
+// Visualizzazione num2
+console.log('Il valore della variabile num2 è ' + num2);
+
+// Dichiarazione Stringa
+let string1 = 'Lorenzo';
+
+// Visualizzazione Stringa
+console.log('Il valore della variabile string1 è ' + string1);
+
+string1 = 'Stefano';
+
+// Visualizzazione Stringa
+console.log('Dopo la variazione, il valore della variabile string1 è ' + string1);
+
+
+// Dichiarazione PIGRECO
+const PIGRECO = 3.14159265359;
+
+// Visualizzazione Stringa
+console.log('Il valore di PIGRECO è ' + PIGRECO);
+
+// Ho provato a cambiare il valore di PIGRECO e il codice 
+// va in eccezione, perchè non si può dichiarare un altro 
+// valore a u a costante.
